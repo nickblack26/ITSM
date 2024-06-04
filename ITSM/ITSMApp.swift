31fc6f473788ff790479e596c36eea93e82aa619
Nick Black <nickblack@VELO-V201849.local>
2024-06-04 11:13:05 -0500

    Initial Commit

---
+++ ITSM/ITSMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITSMApp.swift
+//  ITSM
+//
+//  Created by Nick Black on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ITSMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
